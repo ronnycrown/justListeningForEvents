2ff1daafd643d032582a2a5f9f8366c9f3a1d9a2
Aderonke Odushola <aderonkediana@gmail.com>
2020-05-21 01:10:42 +0100

    this is just to test how to listen to events in javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+function switchToLight(){
+  document.getElementById("light").style.backgroundColor = "#FF0033";
+  document.getElementById("light").style.color = "#fff";
+}
+
+function darkMode(){
+  document.querySelector(".dark").style.backgroundColor = "#000";
+  document.querySelector(".dark").style.color = "#fff";
+}
